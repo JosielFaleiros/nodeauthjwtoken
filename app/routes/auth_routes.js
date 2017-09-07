625902e2d3790c0d6c51f4dbe984f3a25388b9aa
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-09-06 23:26:40 -0300

    Getting user data by token

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -12,7 +12,23 @@ router.use(bodyParser.json())
 /*
 router.use(bodyParser.urlencoded({ extended: true }))
 */
-
+router.get('/profile', (req, res) => {
+    let token = req.headers['x-access-token']
+    if (!token) return res.status(401)
+        .send({ auth: false, 
+        message: 'No token provided.' })
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) return res.status(500)
+            .send({ auth: false, 
+            message: 'Failed to authenticate token.' })
+        User.findById(decoded.id,
+            { password: 0 }, (err, user) => {
+            if (err) return res.status(500).send('There was a problem.')
+            if (!user) return res.status(404).send('No user found.')
+            res.status(200).send(user)
+        })
+    })
+})
 router.post('/register', (req, res) => {
     let hashedPassword = bcrypt.hashSync(req.body.password, 12)
     User.create({
