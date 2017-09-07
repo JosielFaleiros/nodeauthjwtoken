e86e9e2812ceef2ce022f98a0c83ee1f7749eca5
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-09-06 23:51:43 -0300

    Added Login and logout routes with token :D

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -47,5 +47,26 @@ router.post('/register', (req, res) => {
         res.status(200).send({ auth: true, token: token })
     })
 })
+router.post('/login', (req, res) => {
+    console.log(req.body)
+    if (!req.body.email || !req.body.password ) return res.status(402)
+        .send({ message: 'The email and password must be set.' })
+    User.findOne(
+        { email: req.body.email }, (err, user) => {
+        console.log(user)
+        if (err)  return res.status(500).send('Error on the server.')
+        if (!user) return res.status(404).send('No user found.')
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+        if (!passwordIsValid) return res.status(401)
+            .send({ auth: false, token: null })
+        let token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: 3600
+        })
+        res.status(200).send({ auth: true, token: token })
+    })
+})
 
+router.get('/logout', (req, res) => {
+    res.send({ auth: false, token: null })
+})
 module.exports = router
